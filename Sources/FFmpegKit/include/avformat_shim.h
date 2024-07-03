92345894d6ae15aef3d8200f2b016cd940975723
kintan <kingslay@icloud.com>
2024-07-03 20:02:00 +0800

    add func

--- Sources/FFmpegKit/include/avformat_shim.h
+++ Sources/FFmpegKit/include/avformat_shim.h
@@ -21,9 +21,9 @@
 int ff_isom_write_vpcc(AVFormatContext *s, AVIOContext *pb,
                        const uint8_t *data, int len,
                        AVCodecParameters *par);
-//int ff_isom_write_avcc(AVIOContext *pb, const uint8_t *data, int len);
-//int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int size, int ps_array_completeness);
-//int ff_isom_write_av1c(AVIOContext *pb, const uint8_t *buf, int size, int write_seq_header);
+int ff_isom_write_avcc(AVIOContext *pb, const uint8_t *data, int len);
+int ff_isom_write_hvcc(AVIOContext *pb, const uint8_t *data, int size, int ps_array_completeness);
+int ff_isom_write_av1c(AVIOContext *pb, const uint8_t *buf, int size, int write_seq_header);
 //#import <Libavformat/url.h>
 
 typedef struct URLContext {
